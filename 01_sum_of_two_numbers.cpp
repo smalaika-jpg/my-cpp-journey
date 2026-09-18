d8d319447fb694c647ab7e3d564a575782e11d46
smalaika-jpg <smalaika0609@gmail.com>
2026-09-18 14:13:47 +0500

    upload sum of two numbers program

---
+++ 01_sum_of_two_numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1,num2,sum;
+    cout<< "Enter a number you want to be num1: ";
+    cin>>num1;
+
+    cout<< "Enter a number you want to be num2: ";
+    cin>>num2;
+
+    sum=num1+num2;
+    cout<<sum;
+
+    return 0;
+
+}
