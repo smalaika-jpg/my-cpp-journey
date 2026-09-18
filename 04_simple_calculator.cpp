d52405b39e4924d71e1d5cdae6db0001755f5ba2
smalaika-jpg <smalaika0609@gmail.com>
2026-09-18 14:16:52 +0500

    upload simple_calculator program

---
+++ 04_simple_calculator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b,sum,subtraction,multiplication,division;
+	
+	a=7;
+	b=8;
+	
+	sum=a+b;
+	
+	subtraction=a-b;
+	
+	multiplication=a*b;
+	
+	division=a/b;
+	
+	cout<<sum <<endl;
+	cout<<subtraction <<endl;
+	cout<<multiplication <<endl;
+	cout<<division;
+	
+	return 0;
+	
+}
