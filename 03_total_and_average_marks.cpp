f7f664dd53ea6df447ca1f48eed09f9aaf201fdb
smalaika-jpg <smalaika0609@gmail.com>
2026-09-18 14:16:07 +0500

    upload total_and_average_marks program

---
+++ 03_total_and_average_marks.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int marks1,marks2,marks3,marks4,marks5,total;
+    float avg;
+
+    marks1=80;
+    marks2=40;
+    marks3=60;
+    marks4=70;
+    marks5=50;
+
+    total= marks1+marks2+marks3+marks4+marks5;
+    avg=total/5;
+    cout<<total <<endl;
+    cout<<avg;
+
+    return 0;
+
+}
