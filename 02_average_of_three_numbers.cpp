4df1c0e32806e4c60bb7b6fc03e9dd03e7278d7f
smalaika-jpg <smalaika0609@gmail.com>
2026-09-18 14:15:08 +0500

    upload average_of_three_numbers program

---
+++ 02_average_of_three_numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c,sum,avg;
+
+    a=6;
+    b=7;
+    c=12;
+
+    sum=a+b+c;
+    avg=sum/3;
+    cout<<"The average is: " << avg;
+
+    return 0;
+
+}
+
