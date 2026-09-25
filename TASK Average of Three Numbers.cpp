22eb981023735d85efeb733e4f19240f0552aeb1
Malaika Shahid <smalaika0609@gmail.com>
2026-09-25 12:29:56 +0500

    upload TASK_Average_of_Three_Numbers

---
+++ TASK Average of Three Numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	float sum,avg;
+	cout<<"Enter the value of variable a: ";
+	cin>>a;
+	cout<<"Enter the value of variable b: ";
+	cin>>b;
+	cout<<"Enter the value of variable c: ";
+	cin>>c;
+	sum=a+b+c;
+	avg=sum/3;
+	cout<<"Average is = "<<avg;
+	return 0;
+	
+}
